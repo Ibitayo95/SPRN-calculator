85c245ba2a54029bad86847623a97178f952aa6f
ibi <ibitayola@gmail.com>
2021-10-11 18:49:40 +0100

    lots of changes- test 3 almost complete

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,31 @@ class Main {
 
   // main method
   // reads in input from the command line
-  // and passes this input to the processCommand method in SRPN 
+  // and passes this input to the processCommand method in SRPN
 
   public static void main(String[] args) {
-    // Code to take input from the command line 
+    // Code to take input from the command line
     // This input is passed to the processCommand
-    // method in SRPN.java 
+    // method in SRPN.java
     SRPN sprn = new SRPN();
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
+
     try {
-      //Keep on accepting input from the command-line
-      while(true) {
+      // Keep on accepting input from the command-line
+      while (true) {
         String command = reader.readLine();
-        //Close on an End-of-file (EOF) (Ctrl-D on the terminal)
-        if(command == null){
-          //Exit code 0 for a graceful exit
+        // Close on an End-of-file (EOF) (Ctrl-D on the terminal)
+        if (command == null) {
+          // Exit code 0 for a graceful exit
           System.exit(0);
-        }        
-        //Otherwise, (attempt to) process the character
-        sprn.processCommand(command);          
+        }
+        // Otherwise, (attempt to) process the character
+        sprn.processCommand(command);
       }
-    } 
-    catch(IOException e) {
+    } catch (IOException e) {
       System.err.println(e.getMessage());
       System.exit(1);
     }
-  } 
+  }
 }
--- src/SRPN.java
+++ src/SRPN.java
@@ -1,58 +1,90 @@
 import java.util.*;
 
+public class SRPN {
 
- public class SRPN {
+  // creating new stack
+  Stack<Integer> stack = new Stack<>();
 
-  //creating new stack
-Stack <Integer> stack = new Stack<>();
-
-    public void processCommand(String s) {
-      try {
-  //if + sign added then pop the top two from stack and add them together
-        if (s.equals("+")) {
-          int operandA = stack.pop();
-          int operandB = stack.pop();
-          stack.push(operandB + operandA);
-        }
-  //if - sign added then pop the top two from stack and take away
-        else if (s.equals("-")) {
-          int operandA = stack.pop();
-          int operandB = stack.pop();
-          stack.push(operandB - operandA);
-        }
-  //if * sign added then pop the top two from stack and multiply
-        else if (s.equals("*")) {
-          int operandA = stack.pop();
-          int operandB = stack.pop();
-          stack.push(operandB * operandA);
-        }
-  //if % sign added then pop the top two from stack and apply modulo
-        else if (s.equals("%")) {
-          int operandA = stack.pop();
-          int operandB = stack.pop();
-          stack.push(operandB % operandA);
-        }
-  //peek top of stack if equals sign
-        else if (s.equals("=")) { 
-          try {
-            System.out.println(stack.peek());
-        }
-          catch (EmptyStackException e) {
-            System.out.println("Stack empty.");
-        }
-        }
-  //otherwise any number we enter gets pushed into the stack
-        else {
-          int x = Integer.parseInt(s);
-          stack.push(x);
+  public void processCommand(String s) {
+    try {
+      // if + sign added then pop the top two from stack and add them together
+      if (s.equals("+")) {
+        int operandA = stack.pop();
+        int operandB = stack.pop();
+        double saturatedValue = (double) operandB + (double) operandA;
+        stack.push((int)saturatedValue);
+      }
+      // if - sign added then pop the top two from stack and take away
+      else if (s.equals("-")) {
+        int operandA = stack.pop();
+        int operandB = stack.pop();
+        double saturatedValue = (double) operandB - (double) operandA;
+        stack.push((int)saturatedValue);
+      }
+      // if * sign added then pop the top two from stack and multiply
+      else if (s.equals("*")) {
+        int operandA = stack.pop();
+        int operandB = stack.pop();
+        double saturatedValue = (double) operandB * (double) operandA;
+        stack.push((int)saturatedValue);
+      }
+      // if % sign added then pop the top two from stack and apply modulo
+      else if (s.equals("%")) {
+        int operandA = stack.pop();
+        int operandB = stack.pop();
+        double saturatedValue = (double) operandB % (double) operandA;
+        stack.push((int)saturatedValue);
+      }
+      // if / sign added then pop the top two from stack and apply division
+      else if (s.equals("/")) {
+        int operandA = stack.pop();
+        int operandB = stack.pop();
+        double saturatedValue = (double) operandB / (double) operandA;
+        stack.push((int)saturatedValue);
+      }
+      // if entered character is d then print out everything in the stack on a new
+      // line
+      else if (s.equals("d")) {
+        proccessD();
+      }
+      // peek top of stack if equals sign
+      else if (s.equals("=")) {
+        try {
+          System.out.println(stack.peek());
+        } catch (EmptyStackException e) {
+          System.out.println("Stack empty.");
         }
       }
 
-      catch (EmptyStackException x) {
-        System.out.println("Stack underflow.");
+      // otherwise any number we enter gets pushed into the stack
+      // note that taking in as a double then casting to int solves
+      else {
+        double y = Double.parseDouble(s);
+        int x = (int) y;
+        stack.push(x);
       }
-             
-              
-            }   
+    }
 
+    catch (EmptyStackException x) {
+      System.out.println("Stack underflow.");
+    } catch (ArithmeticException y) {
+      System.out.println("Divide by 0.");
+    } catch (NumberFormatException z) {
+      System.out.println("Unrecognised operator or operand \"" + s + "\"");
+    }
+
+  }
+
+  // d prints out everything in the stack on a new line
+  public void proccessD() {
+    if (stack.isEmpty()) {
+      System.out.println(Integer.MIN_VALUE);
+    } else {
+      for (int element : stack) {
+        System.out.println(element);
+      }
+    }
+  }
+
+   
 }
