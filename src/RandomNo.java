a9a2670a5cb424fce05a3bda200c044fa04b0fa9
Ibitayo95 <71972724+Ibitayo95@users.noreply.github.com>
2021-10-15 15:27:02 +0100

    added more stuff

---
+++ src/RandomNo.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+
+public class RandomNo {
+
+    // r prints out next pseudorandom number and adds it to stack
+    static int counter = 0; // set static counter to 0
+    ArrayList<Integer> randomList = new ArrayList<Integer>(); // create array list
+
+    public RandomNo() {
+        /*
+         * constructor that populates array list with pseudorandom numbers (these are
+         * the same as the ones produced by the C library's random() function when the
+         * seed is set to 1)
+         */
+        randomList.add(1804289383);
+        randomList.add(846930886);
+        randomList.add(1681692777);
+        randomList.add(1714636915);
+        randomList.add(1957747793);
+        randomList.add(424238335);
+        randomList.add(719885386);
+        randomList.add(1649760492);
+        randomList.add(596516649);
+        randomList.add(1189641421);
+        randomList.add(1025202362);
+        randomList.add(1350490027);
+        randomList.add(783368690);
+        randomList.add(1102520059);
+        randomList.add(2044897763);
+        randomList.add(1967513926);
+        randomList.add(1365180540);
+        randomList.add(1540383426);
+        randomList.add(304089172);
+        randomList.add(1303455736);
+        randomList.add(35005211);
+        randomList.add(521595368);
+        randomList.add(294702567);
+        randomList.add(1726956429);
+        randomList.add(336465782);
+        randomList.add(861021530);
+        randomList.add(278722862);
+        randomList.add(233665123);
+        randomList.add(2145174067);
+        randomList.add(468703135);
+        randomList.add(1101513929);
+        randomList.add(1801979802);
+        randomList.add(1315634022);
+        randomList.add(635723058);
+        randomList.add(1369133069);
+        randomList.add(1125898167);
+        randomList.add(1059961393);
+        randomList.add(2089018456);
+        randomList.add(628175011);
+        randomList.add(1656478042);
+        randomList.add(1131176229);
+        randomList.add(1653377373);
+        randomList.add(859484421);
+        randomList.add(1914544919);
+        randomList.add(608413784);
+        randomList.add(756898537);
+        randomList.add(1734575198);
+        randomList.add(1973594324);
+        randomList.add(149798315);
+        randomList.add(2038664370);
+        randomList.add(1129566413);
+        randomList.add(184803526);
+        randomList.add(412776091);
+        randomList.add(1424268980);
+        randomList.add(1911759956);
+        randomList.add(749241873);
+        randomList.add(137806862);
+        randomList.add(42999170);
+        randomList.add(982906996);
+        randomList.add(135497281);
+
+    }
+
+    public int getRandomNo() {
+        // method that gets the next random number
+        int myNo = randomList.get(counter);
+        if (counter == randomList.size()) {
+            counter = 0;
+        }
+        counter++;
+        return myNo;
+    }
+}
--- src/SRPN.java
+++ src/SRPN.java
@@ -4,47 +4,65 @@ public class SRPN {
 
   // creating new stack
   Stack<Integer> stack = new Stack<>();
+  
+  
 
   public void processCommand(String s) {
-    // Split the string into an array and loop through it (isolating operators so
-    // they stay in the array)
+    // remove any comments first
+    
+
+    /*
+     * Split the string into an array and loop through it (isolating operators so
+     * they stay in the array) (using regex)
+     */
     for (String digit : s.split("((?=[+|-|*|%|/|^|d|r|=| ])|(?<=[+|-|*|%|/|^|d|r|=| ]))")) {
       try {
-        if (digit.equals("+")) {
-          processPlus();
-        } else if (digit.equals(" ")) {
-          continue;
-        } else if (digit.equals("-")) {
-          processMinus();
-        } else if (digit.equals("*")) {
-          processMultiply();
-        } else if (digit.equals("%")) {
-          processModulo();
-        } else if (digit.equals("/")) {
-          processDivide();
-        } else if (digit.equals("^")) {
-          processPower();
-        } else if (digit.equals("d")) {
-          proccessD();
-        } else if (digit.equals("r")) {
-          processR();
-        } else if (digit.equals("=")) {
-          processEquals();
+        if (stack.size() < 23) {
+
+          if (digit.equals("+")) {
+            processPlus();
+          } else if (digit.equals(" ")) // if there is a space then do nothing
+          {
+            continue;
+          } else if (digit.equals("-")) {
+            processMinus();
+          } else if (digit.equals("*")) {
+            processMultiply();
+          } else if (digit.equals("%")) {
+            processModulo();
+          } else if (digit.equals("/")) {
+            processDivide();
+          } else if (digit.equals("^")) {
+            processPower();
+          } else if (digit.equals("d")) {
+            proccessD();
+          } else if (digit.equals("r")) {
+            RandomNo num = new RandomNo();
+            stack.push(num.getRandomNo());
+          } else if (digit.equals("=")) {
+            processEquals();
+          }
+          /*
+           * otherwise any number we enter gets pushed into the stack note that taking in
+           * the number as a double, then casting to int keeps the numbers saturated
+           */
+          else {
+
+            double y = Double.parseDouble(digit);
+            int x = (int) y;
+            stack.push(x);
+
+          }
+
         }
-        // otherwise any number we enter gets pushed into the stack
-        // note that taking in the number as a double, then casting to int keeps the
-        // numbers saturated
+        // the max stack size is 23 so anything over that should throw an error message
         else {
-          double y = Double.parseDouble(digit);
-          int x = (int) y;
-          stack.push(x);
+          System.err.println("Stack overflow.");
         }
       }
-
+      // error messages
       catch (EmptyStackException x) {
         System.out.println("Stack underflow.");
-      } catch (StackOverflowError w) {
-        System.out.println("Stack overflow.");
       } catch (ArithmeticException y) {
         System.out.println("Divide by 0.");
       } catch (NumberFormatException z) {
@@ -55,7 +73,8 @@ public class SRPN {
   }
 
   // CALCULATION METHODS
-  // if + sign added then pop the top two from stack and add them together
+  // if + sign added then pop the top two from stack and add them together, push
+  // result to the stack
   public void processPlus() {
     if (stack.size() >= 2) {
       int operandA = stack.pop();
@@ -67,7 +86,8 @@ public class SRPN {
     }
   }
 
-  // if - sign added then pop the top two from stack and subtract
+  // if - sign added then pop the top two from stack and subtract, push result to
+  // the stack
   public void processMinus() {
     if (stack.size() >= 2) {
       int operandA = stack.pop();
@@ -79,7 +99,8 @@ public class SRPN {
     }
   }
 
-  // if * sign added then pop the top two from stack and multiply
+  // if * sign added then pop the top two from stack and multiply, push result to
+  // the stack
   public void processMultiply() {
     if (stack.size() >= 2) {
       int operandA = stack.pop();
@@ -91,7 +112,8 @@ public class SRPN {
     }
   }
 
-  // if % sign added then pop the top two from stack and apply modulo
+  // if % sign added then pop the top two from stack and apply modulo, push result
+  // to the stack
   public void processModulo() {
     if (stack.size() >= 2) {
       int operandA = stack.pop();
@@ -103,7 +125,8 @@ public class SRPN {
     }
   }
 
-  // if / sign added then pop the top two from stack and apply division
+  // if / sign added then pop the top two from stack and apply division, push
+  // result to the stack
   public void processDivide() {
     if (stack.size() >= 2) {
       int operandA = stack.pop();
@@ -115,12 +138,29 @@ public class SRPN {
     }
   }
 
+  /*
+   * slightly different as we get an error message if the second operand is
+   * negative. Pop the top two and perform operandB ^ operand A. if operand A is
+   * negative then print error message and push them back to the stack
+   */
   public void processPower() {
     if (stack.size() >= 2) {
       int operandA = stack.pop();
       int operandB = stack.pop();
-      double saturatedValue = Math.pow((double) operandB, (double) operandA);
-      stack.push((int) saturatedValue);
+
+      if (operandA >= 0) {
+
+        double saturatedValue = Math.pow((double) operandB, (double) operandA);
+        stack.push((int) saturatedValue);
+      }
+
+      else {
+        System.err.println("Negative power.");
+        stack.push(operandB);
+        stack.push(operandA);
+
+      }
+
     } else {
       throw new EmptyStackException();
     }
@@ -144,12 +184,6 @@ public class SRPN {
         System.out.println(element);
       }
     }
-  }
-  // r prints out next pseudorandom number and adds it to stack?
-  public void processR(){
-    Random number = new Random(1);
-    double randNum = number.nextInt(Integer.MAX_VALUE);
-    stack.push((int) randNum);
   }
 
 }
